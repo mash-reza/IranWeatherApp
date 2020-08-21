82b90a7bb989af79d3b1bc4b9021254a938b5b35
Festive <27864684+mash-reza@users.noreply.github.com>
2020-08-21 09:43:01 +0430

    retrofit

--- app/src/main/java/com/festive/iranweatherapp/di/AppModule.java
+++ app/src/main/java/com/festive/iranweatherapp/di/AppModule.java
@@ -1,8 +1,23 @@
 package com.festive.iranweatherapp.di;
 
 import dagger.Module;
+import dagger.Provides;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
-public class AppModule {
+public abstract class AppModule {
 
+    @Provides
+    public static Retrofit provideRetrofit() {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        return new Retrofit.Builder()
+                .baseUrl("")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(new OkHttpClient().newBuilder().addInterceptor(loggingInterceptor).build())
+                .build();
+    }
 }
